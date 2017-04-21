c9baedb1fe553587d6bd9bef7f8c9214bcd9eaa9
Fed Reggiardo <bksgtr@gmail.com>
2017-04-21 10:46:10 -0500

    Reorganized a few files

---
+++ Assets/Views/MainViews.elm
@@ -0,0 +1,89 @@
+module Assets.Views.MainViews exposing (mainView)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+import String exposing (..)
+import Assets.Logic.Routing as Routing
+import Assets.Logic.Types exposing (Model, Msg(..), Route(..), PlayBundle)
+import Assets.Logic.Audio exposing (noteSorter)
+import Assets.Views.Home exposing (homePage)
+import Assets.Views.Chords exposing (chordChartPage, keyList)
+import Assets.Views.Scales exposing (scalesPage)
+import Assets.Views.Fretboard exposing (fretboardPage, noteStringPos, noteFretPos)
+import Assets.Views.Strum exposing (strumPage)
+import Assets.Styles.MainStyles exposing (..)
+import InlineHover exposing (hover)
+
+
+mainView : Model -> Html Msg
+mainView model =
+    div [ style [ ( "position", "relative" ), ( "overflow", "hidden" ) ] ]
+        [ nav model
+        , modalIcon model
+        , page model
+        ]
+
+
+nav : Model -> Html Msg
+nav model =
+    div [ navMenuStyle model ]
+        [ navIcon model
+        , div []
+            [ hover highlight a [ href Routing.homePath, navItemStyle ] [ text "HOME" ]
+            , hover highlight a [ href (Routing.scalesPath model.musKey), navItemStyle ] [ text "SCALES" ]
+            , hover highlight a [ href (Routing.chordsPath model.musKey), navItemStyle ] [ text "CHORDS" ]
+            , hover highlight a [ href (Routing.fretboardPath model.musKey), navItemStyle ] [ text "FRETBOARD" ]
+            , hover highlight a [ href Routing.strumPath, navItemStyle ] [ text "STRUMMING" ]
+            , div [ style [ ( "marginTop", "100px" ), ( "color", "#E91750" ) ] ] [ text "SELECT KEY:" ]
+            , keyListView model
+            ]
+        ]
+
+
+navIcon : Model -> Html Msg
+navIcon model =
+    div [ navIconStyle model, onClick ShowNavMenu ]
+        [ hr [ navIconStyleHr ] []
+        , hr [ navIconStyleHr ] []
+        , hr [ navIconStyleHr ] []
+        ]
+
+
+keyListView : Model -> Html Msg
+keyListView model =
+    let
+        keyOptions key =
+            hover highlight span [ keyListStyle, onClick <| ChangeKey key ] [ text key ]
+    in
+        div [ keyListContainerStyle ]
+            [ div [ textContainerStyle ]
+                (List.map keyOptions keyList)
+            ]
+
+
+modalIcon : Model -> Html Msg
+modalIcon model =
+    div [ modalIconStyle model, onClick ShowModal ] [ text "?" ]
+
+
+page : Model -> Html Msg
+page model =
+    case model.route of
+        ChordChartPage key ->
+            chordChartPage model
+
+        FretboardPage key ->
+            fretboardPage model
+
+        ScalesPage key ->
+            scalesPage model
+
+        HomePage ->
+            homePage model
+
+        StrumPage ->
+            strumPage model
+
+        NotFoundPage ->
+            div [ style [ ( "margin", "100px auto" ), ( "color", "#E91750" ) ] ] [ text ("Page Not Found " ++ model.musKey) ]
--- Main.elm
+++ Main.elm
@@ -1,24 +1,15 @@
 port module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
 import List.Extra exposing (getAt)
 import Assets.Logic.Routing as Routing
 import Assets.Logic.Types exposing (Model, Msg(..), Route(..), PlayBundle)
 import Assets.Logic.Audio exposing (noteSorter)
-import Assets.Views.Home exposing (homePage)
-import Assets.Views.Chords exposing (chordChartPage, keyList)
-import Assets.Views.Scales exposing (scalesPage)
 import Assets.Views.Fretboard exposing (fretboardPage, noteStringPos, noteFretPos)
-import Assets.Views.Strum exposing (strumPage)
+import Assets.Views.MainViews exposing (mainView)
 import Navigation exposing (Location)
 import Time exposing (..)
 import Update.Extra.Infix exposing ((:>))
-import Json.Decode exposing (..)
-import InlineHover exposing (hover)
 import Random exposing (..)
-import Assets.Styles.MainStyles exposing (..)
 
 
 main =
@@ -57,19 +48,19 @@ port send : PlayBundle -> Cmd msg
 update msg model =
     case msg of
         NoOp ->
-            ( model, Cmd.none )
+            model ! []
 
         ShowNavMenu ->
-            ( { model | navMenuOpen = not model.navMenuOpen, modalOpen = False }, Cmd.none )
+            { model | navMenuOpen = not model.navMenuOpen, modalOpen = False } ! []
 
         ShowModal ->
-            ( { model | modalOpen = not model.modalOpen }, Cmd.none )
+            { model | modalOpen = not model.modalOpen } ! []
 
         Randomize ->
             ( model, Random.generate StrumArrowDirection <| Random.list 8 (Random.int 1 2) )
 
         StrumArrowDirection numList ->
-            ( { model | strumArrow = numList }, Cmd.none )
+            { model | strumArrow = numList } ! []
 
         OnLocationChange location ->
             let
@@ -79,20 +70,19 @@ update msg model =
                 newKey =
                     Maybe.withDefault "C" <| Routing.modelUpdateOnHash model location
             in
-                ( { model | route = newRoute, musKey = newKey, modalOpen = False }, Cmd.none )
+                { model | route = newRoute, musKey = newKey, modalOpen = False } ! []
 
         ChangeKey key ->
-            ( { model | musKey = key, navMenuOpen = False }
-            , Cmd.none
-            )
+            { model | musKey = key, navMenuOpen = False } ! []
 
         Play chord hzShift ->
-            ( { model | currentChord = chord, pitchShift = hzShift }, Cmd.none )
+            { model | currentChord = chord, pitchShift = hzShift }
+                ! []
                 :> update ResetIndex
                 :> update SendNotes
 
         ResetIndex ->
-            ( { model | index = 0 }, Cmd.none )
+            { model | index = 0 } ! []
 
         SendNotes ->
             let
@@ -117,14 +107,14 @@ update msg model =
                 finalOffset =
                     fretOffset + stringOffset
             in
-                ( { model | notePosition = finalOffset, showAccidental = accidental }, Cmd.none )
+                { model | notePosition = finalOffset, showAccidental = accidental } ! []
 
         ChangeSliderValue newVal ->
             let
                 val =
                     Result.withDefault 1 <| String.toInt newVal
             in
-                ( { model | sliderValue = val }, Cmd.none )
+                { model | sliderValue = val } ! []
 
 
 subscriptions : Model -> Sub Msg
@@ -137,76 +127,3 @@ subscriptions model =
             Time.every (val * Time.second) (always SendNotes)
         else
             Sub.none
-
-
-mainView : Model -> Html Msg
-mainView model =
-    div [ style [ ( "position", "relative" ), ( "overflow", "hidden" ) ] ]
-        [ nav model
-        , modalIcon model
-        , page model
-        ]
-
-
-nav : Model -> Html Msg
-nav model =
-    div [ navMenuStyle model ]
-        [ navIcon model
-        , div []
-            [ hover highlight a [ href Routing.homePath, navItemStyle ] [ text "HOME" ]
-            , hover highlight a [ href (Routing.scalesPath model.musKey), navItemStyle ] [ text "SCALES" ]
-            , hover highlight a [ href (Routing.chordsPath model.musKey), navItemStyle ] [ text "CHORDS" ]
-            , hover highlight a [ href (Routing.fretboardPath model.musKey), navItemStyle ] [ text "FRETBOARD" ]
-            , hover highlight a [ href Routing.strumPath, navItemStyle ] [ text "STRUMMING" ]
-            , div [ style [ ( "marginTop", "100px" ), ( "color", "#E91750" ) ] ] [ text "SELECT KEY:" ]
-            , keyListView model
-            ]
-        ]
-
-
-navIcon : Model -> Html Msg
-navIcon model =
-    div [ navIconStyle model, onClick ShowNavMenu ]
-        [ hr [ navIconStyleHr ] []
-        , hr [ navIconStyleHr ] []
-        , hr [ navIconStyleHr ] []
-        ]
-
-
-keyListView : Model -> Html Msg
-keyListView model =
-    let
-        keyOptions key =
-            hover highlight span [ keyListStyle, onClick <| ChangeKey key ] [ text key ]
-    in
-        div [ keyListContainerStyle ]
-            [ div [ textContainerStyle ]
-                (List.map keyOptions keyList)
-            ]
-
-
-page : Model -> Html Msg
-page model =
-    case model.route of
-        ChordChartPage key ->
-            chordChartPage model
-
-        FretboardPage key ->
-            fretboardPage model
-
-        ScalesPage key ->
-            scalesPage model
-
-        HomePage ->
-            homePage model
-
-        StrumPage ->
-            strumPage model
-
-        NotFoundPage ->
-            div [ style [ ( "margin", "100px auto" ), ( "color", "#E91750" ) ] ] [ text ("Page Not Found " ++ model.musKey) ]
-
-
-modalIcon : Model -> Html Msg
-modalIcon model =
-    div [ modalIconStyle model, onClick ShowModal ] [ text "?" ]
